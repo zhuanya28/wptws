0ab36689e7e3202b0647b16a3d691b32e12e6fee
anya <azhukova1402@gmail.com>
2024-09-23 19:29:50 +0800

    edited: cube & sphere

--- week3-inclass/js/script-p5.js
+++ week3-inclass/js/script-p5.js
@@ -2,6 +2,8 @@ function setup() {
   let canvas = createCanvas(640, 400);
   canvas.parent("container-p5");
   canvas.hide();
+
+  initThree();
 }
 
 function draw() {
---
+++ week3-inclass/js/script-three.js
@@ -0,0 +1,66 @@
+let container;
+let scene, camera, renderer;
+
+let cube;
+let ball;
+
+
+function initThree() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  container = document.getElementById("container-three");
+  container.appendChild(renderer.domElement);
+
+  setupThree();
+
+}
+
+function setupThree() {
+  // setup like p5
+
+  camera.position.z = 5;
+
+
+  cube = getCube();
+  scene.add(cube);
+
+  ball = getSphere();
+  scene.add(ball);
+
+  renderer.setAnimationLoop(animate);
+}
+
+function updateThree() {
+//
+}
+
+
+function animate() {
+  updateThree();
+  renderer.render(scene, camera);
+
+
+}
+
+
+function getCube() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+function getSphere() {
+  const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffff00
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
