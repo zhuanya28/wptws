10ffac7e64e2c3b30975d414acbf890d452f3de4
anya <azhukova1402@gmail.com>
2024-10-05 11:49:53 +0800

    added: week 3 mini proj boiler plate

---
+++ week3-home/script-p5.js
@@ -0,0 +1,13 @@
+function setup() {
+    let canvas = createCanvas(640, 400);
+    canvas.parent("container-p5");
+    canvas.hide();
+  
+    initThree();
+  }
+  
+  function draw() {
+    background(220);
+    noLoop();
+  }
+  
