0deb90f4646c5e1717437441072cbd425f375c75
anya <azhukova1402@gmail.com>
2024-10-05 20:18:31 +0800

    week3 hw: moving rects

--- week3-home/main.js
+++ week3-home/main.js
@@ -5,6 +5,7 @@ let light, lightMesh;
 let sculpture;
 let cubes = [];
 let rectNum = 10;
+let lathe;
 
 function setupThree() {
   // It is not recommended to explore materials and lights this week!
@@ -26,32 +27,39 @@ function setupThree() {
   lightMesh.scale.set(10, 10, 10);
 
   // add meshes
-  for (let i = 0; i < rectNum; i++){
+  for (let i = 0; i < rectNum; i++) {
     cubes[i] = getCube();
-    cubes[i].position.set(0, i*15, 0);
-    cubes[i].scale.set(200, i, 100);
+    cubes[i].position.set(0, i * 15, 0);
+    cubes[i].scale.set(200 * Math.random(), i * Math.random()*5, 100 * Math.sin(i));
   }
+
+  lathe = getLathe();
+
+
   // change color
-//   ball.material.color.set("#FF00FF");
-//   // change transparency
-//   ball.material.transparent = true;
-//   ball.material.opacity = 0.75;
+  //   ball.material.color.set("#FF00FF");
+  //   // change transparency
+  //   ball.material.transparent = true;
+  //   ball.material.opacity = 0.75;
 
   sculpture = new THREE.Group();
   scene.add(sculpture);
-  for (let i = 0; i < rectNum; i++){
+  for (let i = 0; i < rectNum; i++) {
     sculpture.add(cubes[i]);
   }
+  sculpture.add(lathe);
+
+
 }
 
-function getCube(){
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshPhongMaterial({
-      color: "#999999",
-      shininess: 100
-    });
-    const mesh = new THREE.Mesh(geometry, material);
-    return mesh;
+function getCube() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshPhongMaterial({
+    color: "#999999",
+    shininess: 100,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
 }
 
 function updateThree() {
@@ -61,13 +69,14 @@ function updateThree() {
   let y = 300;
   let z = sin(angle) * radDist;
   light.position.set(x, y, z);
-}
 
+  sculpture.rotation.y = frame * 0.01;
+}
 
 function getBasicSphere() {
   const geometry = new THREE.SphereGeometry(1, 32, 32);
   const material = new THREE.MeshBasicMaterial({
-    color: "#ffffff"
+    color: "#ffffff",
   });
   const mesh = new THREE.Mesh(geometry, material);
   return mesh;
@@ -78,7 +87,13 @@ function getPointLight(color) {
   return light;
 }
 
-
-
-
-
+function getLathe() {
+  const points = [];
+  for (let i = 0; i < 10; i++) {
+    points.push(new THREE.Vector2(Math.sin(i * 0.2) * 10 + 5, (i - 5) * 2));
+  }
+  const geometry = new THREE.LatheGeometry(points);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  const lathe = new THREE.Mesh(geometry, material);
+  return lathe;
+}
