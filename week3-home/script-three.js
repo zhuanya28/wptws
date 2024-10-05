6f9909d6a8838e879c7f3787c694df1467aad80d
anya <azhukova1402@gmail.com>
2024-10-05 12:04:50 +0800

    fixed: main file & example for hw

--- week3-home/script-three.js
+++ week3-home/script-three.js
@@ -1,71 +1,43 @@
-let container;
-let scene, camera, renderer;
+console.log("three.js Version: " + THREE.REVISION);
+
+let scene, camera, renderer, container;
 let controls;
-
-let cube;
-let ball;
-
+let time, frame = 0;
 
 function initThree() {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  const fov = 75;
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  const near = 0.1;
+  const far = 10000;
+  camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+  camera.position.z = 1000;
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-
-
 
   container = document.getElementById("container-three");
   container.appendChild(renderer.domElement);
 
   controls = new OrbitControls(camera, renderer.domElement);
 
-  setupThree();
-
-}
-
-function setupThree() {
-  // setup like p5
-
-  camera.position.z = 5;
-
-
-  cube = getCube();
-  scene.add(cube);
-
-  ball = getSphere();
-  scene.add(ball);
+  setupThree(); // *** 
 
   renderer.setAnimationLoop(animate);
 }
 
-function updateThree() {
-//
-}
-
-
 function animate() {
-  updateThree();
+  time = performance.now();
+  frame++;
+
+  updateThree(); // ***
+
   renderer.render(scene, camera);
-
-
 }
 
-
-function getCube() {
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshNormalMaterial({
-    color: 0x00ff00
-  });
-  const mesh = new THREE.Mesh(geometry, material);
-  return mesh;
-}
-
-function getSphere() {
-  const geometry = new THREE.SphereGeometry( 15, 32, 16 );
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xffff00
-  });
-  const mesh = new THREE.Mesh(geometry, material);
-  return mesh;
-}
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
